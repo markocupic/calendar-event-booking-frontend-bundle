bfe45e99177da3bcd0a3ca259950210388c96c8a
Marko Cupic <m.cupic@gmx.ch>
2019-11-29 20:04:51 +0100

    initial commit

---
+++ src/MarkocupicCalendarEventBookingFrontendBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright Marko Cupic <m.cupic@gmx.ch>, 2019
+ * @author Marko Cupic
+ * @link https://github.com/markocupic/dummy-bundle
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Markocupic\CalendarEventBookingFrontendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class MarkocupicCalendarEventBookingFrontendBundle
+ * @package Markocupic\CalendarEventBookingFrontendBundle
+ */
+class MarkocupicCalendarEventBookingFrontendBundle extends Bundle
+{
+}
---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Copyright Marko Cupic <m.cupic@gmx.ch>, 2019
+ * @author Marko Cupic
+ * @link https://github.com/markocupic/dummy-bundle
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+// contao/dca/tl_module.php
+$GLOBALS['TL_DCA']['tl_module']['palettes']['dummy_module'] = '{title_legend},name,headline,type;{redirect_legend},jumpTo';
