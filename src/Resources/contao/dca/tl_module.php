9296d17bf2abb3064ca634ea88f224cd64235a90
Marko Cupic <m.cupic@gmx.ch>
2019-11-29 22:55:37 +0100

    dev

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,12 +1,31 @@
 <?php
+
 /*
  * Copyright Marko Cupic <m.cupic@gmx.ch>, 2019
  * @author Marko Cupic
- * @link https://github.com/markocupic/dummy-bundle
+ * @link https://github.com/markocupic/calendar-event-booking-frontend-bundle
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
+// Set palettes
+$GLOBALS['TL_DCA']['tl_module']['palettes']['calendar_event_booking_member_list'] = '{title_legend},name,headline,type;{template_legend},calendar_event_booking_member_list_partial_template,calendar_event_booking_member_list_template;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 
-// contao/dca/tl_module.php
-$GLOBALS['TL_DCA']['tl_module']['palettes']['dummy_module'] = '{title_legend},name,headline,type;{redirect_legend},jumpTo';
+// Fields
+$GLOBALS['TL_DCA']['tl_module']['fields']['calendar_event_booking_member_list_template'] = array
+(
+    'exclude'                 => true,
+    'inputType'               => 'select',
+    'options_callback'        => array('markocupic.calendar_event_booking_frontend_bundle.contao.dca.tl_module', 'getCalendarEventBookingMemberListTemplate'),
+    'eval'                    => array('tl_class'=>'w50'),
+    'sql'                     => "varchar(64) NOT NULL default 'mod_calendar_event_booking_member_list'"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['calendar_event_booking_member_list_partial_template'] = array
+(
+    'exclude'                 => true,
+    'inputType'               => 'select',
+    'options_callback'        => array('markocupic.calendar_event_booking_frontend_bundle.contao.dca.tl_module', 'getCalendarEventBookingMemberListPartialTemplate'),
+    'eval'                    => array('tl_class'=>'w50'),
+    'sql'                     => "varchar(64) NOT NULL default 'calendar_event_booking_member_list_partial'"
+);
